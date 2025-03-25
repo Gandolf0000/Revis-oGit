7b1fd752afec0950602bc4ec78503ea3d7a473b7
Gandolf0000 <joaovitor15oliveiracarvalho@gmail.com>
2025-03-25 14:11:01 +0000

    a

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+var nome = "João Vitor"
+const idade = 17
+let signo = "Libra"
+
+/*
+Let é para evitar os erros de escopo, const é para valores constantes que não mudam e var é a variável mais comum
+*/
